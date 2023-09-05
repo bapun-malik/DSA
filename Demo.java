58794eed1bacf01d844f461e6e9e2270b5f17311
bapun <bapunmalik.social@gmail.com>
2023-09-05 10:11:49 +0530

    get update

--- Demo.java
+++ Demo.java
@@ -4,17 +4,5 @@ public class Demo {
         ll.insertFirst(5);
         ll.insertLast(10);
         ll.insertLast(20);
-        ll.insertLast(30);
-        ll.insertLast(50);
-        ll.insertFirst(0);
-        ll.insertAt(3, 40);
-        ll.display();
-        System.out.println("\nSize:"+ll.getSize());
-        ll.deleteFirst();
-        ll.deleteFirst();
-        ll.deleteLast();
-        ll.display();
-        System.out.println("\nSize:"+ll.getSize());
-
     }
 }
